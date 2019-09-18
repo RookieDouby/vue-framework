57b1712d74bba9d67e7c147d8d9b67c832a8de0b
rookiesam <zhd1109@qq.com>
2019-09-18 10:02:09 +0800

    初步get post 封装

--- src/request/http.js
+++ src/request/http.js
@@ -94,3 +94,26 @@ axios.interceptors.response.use(
         }
     }
 )
+
+export function get(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: params
+        }).then(res => {
+            resolve(res.data);
+        }).catch(err => {
+            reject(err.data);
+        })
+    })
+}
+
+export function post(url, params) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, QS.stringify(params))
+            .then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+            })
+    })
+}
